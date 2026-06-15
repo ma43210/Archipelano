ad46a90ead72406b0781e4665cd008717bb10b67
ma43210 <ma43210.mtl@gmail.com>
2026-06-15 15:46:46 -0400

    YAY ITS DONE

--- Assets/Scripts/game_manager.gd
+++ Assets/Scripts/game_manager.gd
@@ -8,6 +8,10 @@ var key = 0
 var all_keys = []
 var gamestart = true
 var exit_is_open = false
+var win = false
+var music: AudioStreamPlayer
+var background_music = preload("res://Assets/Sounds/Music/Fens-of-Fog.mp3")
+
 func randomize_levels():
 	var all_levels = ["1A", "1B", "2A", "2B", "3A", "3B", "4A", "4B", "5A", "5B"]
 	all_levels.shuffle()
@@ -24,6 +28,12 @@ func _ready():
 	Archipelago.connected.connect(connect_script)
 	randomize_levels()
 	reset_key()
+	music = AudioStreamPlayer.new()
+	add_child(music)
+
+func play_music(track: AudioStream):
+	music.stream = track
+	music.play()
 	
 	
 func connect_script(_conn: ConnectionInfo, _json: Dictionary) -> void:
@@ -31,7 +41,7 @@ func connect_script(_conn: ConnectionInfo, _json: Dictionary) -> void:
 	Archipelago.set_client_status(Archipelago.ClientStatus.CLIENT_PLAYING)
 	archipelago = true
 	get_tree().change_scene_to_file("res://Assets/Scenes/Levels/Menu.tscn")
-#Level locations will be #
+
 func get_item(item: NetworkItem):
 	self.all_keys.append(int(item.id))
 
@@ -39,11 +49,11 @@ func start_game():
 	current_level = level_list[current_level_in_array]
 	var full_path = level_path + "level_" + current_level + ".tscn"
 	get_tree().change_scene_to_file(full_path)
+	play_music(background_music)
 
 func next_level():
 	if current_level_in_array == 9:
-		WinnerisYou.you_win()
-		Animations.winner()
+		win = true
 		if archipelago:
 			Archipelago.collect_location(10000)
 			Archipelago.set_client_status(Archipelago.ClientStatus.CLIENT_GOAL)
@@ -62,7 +72,7 @@ func set_up_level():
 	reset_key()
 
 
-#Key locations will be 10#
+
 func add_key():
 	if archipelago:
 		Archipelago.collect_location(current_level_in_array + 101)
